d6d0b79fbab884018afe536d2fd20bb04bbb392b
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-06-05 14:17:42 +0700

    Time: 2 ms (7.87%) | Memory: 17.9 MB (64.23%) - LeetSync

---
+++ 68-text-justification/text-justification.py
@@ -0,0 +1,50 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        n = len(words)
+        curline = 0
+        ans = []
+        i = 0
+        while i < n:
+            backpack = maxWidth
+            j = i
+            while j < n and backpack - len(words[j]) - (j - i) >= 0:
+                backpack-= len(words[j])
+                j+=1
+            temp = ''
+            left = backpack
+            numspace = j - i - 1
+            if numspace !=0:
+                minspace = left//numspace
+            print(left,numspace)
+            if j != n:
+                for o in range( i , j ):
+                    temp+=words[o]
+                    if numspace == 0:
+                        temp+=(' '*left)
+                        break
+                    if o == j -1:
+                        continue
+                    if left%(numspace):
+                        temp+=(' '*(minspace+1))
+                        left -= (minspace + 1)
+                        numspace-=1
+                    else:
+                        temp+=(' '*(minspace))
+                        left -= minspace
+                        numspace-=1
+                    print(left)
+            else:
+                for o in range(i , j):
+                    temp += words[o]
+                    if left > 0:
+                        temp += ' '
+                        left-=1
+                temp+= ' '*left
+            ans.append(temp)
+            i = j
+        return ans
+
+            
+
+
+        
