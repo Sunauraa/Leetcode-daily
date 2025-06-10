fad8937a5f7abdff6258cf240d9d2e40127915a2
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-06-10 07:51:26 +0700

    Time: 3 ms (64.78%) | Memory: 18 MB (6.36%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,31 +1,19 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
         m = defaultdict(int)
-        minodd = 0
-        mineve = 0
-        maxodd = 100
-        maxeve = 100
+        minodd = 100
+        mineve = 100
+        maxodd = 0
+        maxeve = 0
         for i in s:
             m[i] += 1
         for i in 'qwertyuiopasdfghjklzxcvbnm':
             if m[i]:
                 if m[i]%2:
-                    if minodd == 0:
-                        minodd = i
-                    if maxodd == 100:
-                        maxodd == i
-                    if m[i] < m[minodd]:
-                        minodd = i
-                    if m[i] > m[maxodd]:
-                        maxodd = i
+                    maxodd = max(maxodd, m[i])
+                    minodd = min(minodd, m[i])
                 else:
-                    if mineve == 0:
-                        mineve = i
-                    if maxeve == 100:
-                        maxeve == i
-                    if m[i] < m[mineve]:
-                        mineve = i
-                    if m[i] > m[maxeve]:
-                        maxeve = i
-        return m[maxodd]- m[mineve]
+                    maxeve = max(maxeve, m[i])
+                    mineve = min(mineve, m[i])
+        return maxodd - mineve
         
