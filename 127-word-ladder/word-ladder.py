01975cdbbd495c86b378515d5314e50f4c10159c
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-07 17:23:33 +0700

    Time: 7424 ms (7.55%) | Memory: 22 MB (20.47%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -2,30 +2,19 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         queue = deque( [(beginWord,0)] )
         ans = float('inf')
-        vi = defaultdict(int)
+        wordList = set(wordList)
+        vi = set()
 
-        def dif(u,v):
-            count = 1
-            for x,y in zip(u,v):
-                if x != y:
-                    count-=1
-                    if count < 0:
-                        return False
-            return count == 0
-
-        n = len(wordList)
         while queue:
-            #print(queue)
             u,step = queue.popleft()
+            vi.add(u)
             if u == endWord:
                 return step + 1
-            temp = 0
-            while temp < n:
-                if dif(wordList[temp],u):
-                    queue.append((wordList[temp], step+1))
-                    wordList.remove(wordList[temp])
-                    n-=1
-                else:
-                    temp+=1
+            for i in range(len(beginWord)):
+                for x in 'qwertyuiopasdfghjklzxcvbnm':
+                    if u[i] == x:
+                        continue
+                    temp = u[:i] + x + u[i + 1:]
+                    if temp in wordList and temp not in vi:
+                        queue.append((temp, step+1))
         return 0
-            
