87693925b9eeb82446a054c98be2e086ea5bac32
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-07 10:37:20 +0700

    Time: 99 ms (97.60%) | Memory: 54.4 MB (14.96%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        n = len(events)
-        parent = [ i for i in range(10**5+2) ]
         events.sort(key = lambda x: x[1])
-        #print(events)
+        parent = [ i for i in range(events[-1][1]+2) ]
 
         def findparent( u ):
             if parent[u] == u:
                 return u
             parent[u] = findparent(parent[u])
             return parent[u]
-        
-        def union(a,b):
-            a = findparent(a)
-            b = findparent(b)
-            if a > b: a,b = b,a
-            parent[a] = b
 
         res = 0
         for i in events:
