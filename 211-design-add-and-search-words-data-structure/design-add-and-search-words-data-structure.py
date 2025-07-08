0025c6912ee8ca04b80ac720b0067686dee9941d
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-08 13:38:34 +0700

    Time: 2602 ms (6.30%) | Memory: 78.3 MB (6.05%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.isLeaf = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            index = ord(c) - ord('a')
+            if not curr.children[index]:
+                curr.children[index] = TrieNode()
+            curr = curr.children[index]
+        curr.isLeaf = True   
+
+    def search(self, word: str) -> bool:
+        def dfs(curr,word):
+            if not curr:
+                return
+            print(word,curr.isLeaf)
+            if curr.isLeaf and not word:
+                return True
+            for i in range(len(word)):
+                #print(word[i])
+                if word[i] != '.':
+                    index = ord(word[i]) - ord('a')
+                    if not curr.children[index]:
+                        #print(word,word[i])
+                        return False
+                    curr = curr.children[index]
+                else:
+                    #print(word,word[i])
+                    for x in curr.children:
+                        if x:
+                            if dfs(x, word[i+1:]):
+                                return True
+                    return False
+            return curr.isLeaf
+        curr = self.root
+        return dfs(curr,word)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
