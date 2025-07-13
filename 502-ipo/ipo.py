47730f6b457ccb2af43ece7a1291ac34b16e18ad
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-14 00:14:08 +0700

    Time: 235 ms (95.50%) | Memory: 49.2 MB (5.09%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,6 +1,5 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        #print(len(profits))
         m = defaultdict(list)
         for u,v in zip(profits, capital):
             m[v].append(u)
@@ -9,18 +8,12 @@ class Solution:
         cur = 0
         key = sorted(list(m.keys()))
         while k > 0:
-            #print(q)
-            #print(len(key),cur,key[cur])
             while cur < len(key) and key[cur] <= w:
-                #print(2)
                 for x in m[key[cur]]:
                     heapq.heappush(q, -x)
                 cur +=1
             if q:
-                #print(1)
                 temp = heapq.heappop(q)
-                #print(temp)
                 w -= temp
-            #print(w)
             k-=1
         return w
