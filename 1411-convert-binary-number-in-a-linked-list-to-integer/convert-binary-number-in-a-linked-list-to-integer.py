5aeb583cebe75b9c8b4d3a9f7d4d2e13fdd4fe1c
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-14 11:00:21 +0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.17%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
@@ -6,7 +6,6 @@
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
         ans = ''
-        cur = 1
         while head:
             if head.val == 1:
                 ans+='1'
@@ -14,8 +13,8 @@ class Solution:
                 ans+='0'
             head = head.next
         res = 0
+        n = len(ans)
         for i in range(len(ans)-1,-1,-1):
             if ans[i] == '1':
-                res+=cur
-            cur*=2
+                res+=(1<<(n-i-1))
         return res
