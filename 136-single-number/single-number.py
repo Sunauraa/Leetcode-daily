aefa75521829757b03b13bea6fb19c2a2839cd84
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-14 10:50:56 +0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (77.43%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        store = set()
-        ans = 0
+        res = 0
         for i in nums:
-            if i not in store:
-                store.add(i)
-                ans+=i
-            else:
-                ans-=i
-        return ans
+            res = res^i
+        return res
