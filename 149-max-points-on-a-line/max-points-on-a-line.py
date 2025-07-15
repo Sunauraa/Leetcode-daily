086ee68ed5b2c06c0f67ada52e0764406009c6fe
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-15 23:36:19 +0700

    Time: 2177 ms (5.01%) | Memory: 17.8 MB (91.63%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -2,8 +2,6 @@ class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         ans = 1
         n = len(points)
-        if 1.0000000000000002 == 1:
-            return 0
         def linear(u,v):
             if u[1] - v[1] == 0:
                 a = 0
@@ -17,17 +15,12 @@ class Solution:
         for i in range(n):
             for j in range(i+1,n):
                 a,b = linear(points[i],points[j])
-                #print(points[i],points[j],a,b)
                 count = 2
                 for o in range(j+1,n):
                     if a == float('inf'):
                         if points[o][0] == points[i][0]:
                             count+=1
                     elif round(a*(points[o][0]) + b,8) == points[o][1]:
-                        #print(points[o])
                         count+=1
-                    """if a == 3.0434782608695654 and b == 9.0:
-                        print(points[o], a*(points[o][0]) + b,count)"""
-                #print(count)
                 ans = max(count,ans)
         return ans
