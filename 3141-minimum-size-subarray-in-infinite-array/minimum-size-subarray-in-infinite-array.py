ec088146e41af2f9f56223719a6c811c0b1e8677
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-19 15:45:23 +0700

    Time: 78 ms (41.94%) | Memory: 27.4 MB (31.72%) - LeetSync

---
+++ 3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        nums = nums + nums
+        sm = 0
+        n = len(nums)
+        for i in range(n//2):
+            sm+=nums[i]
+        arr = [0]*n
+        res = 0
+        res += (n//2)*(target//sm)
+        target%=sm
+        l = -1
+        r = 0
+        ans = float('inf')
+        """print(nums)
+        print(arr)
+        print(target,res)
+        print()"""
+        temp = 0
+        r = 0
+        for l in range(n):
+            while r < n and temp < target:
+                temp += nums[r]
+                r+=1
+            #print(l,temp,r)
+            if temp == target:
+                #print(l,r)
+                ans = min(ans, r - l)
+            temp-= nums[l]
+        if ans == float('inf'):
+            return -1
+        else:
+            return ans + res
