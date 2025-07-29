8498e15d150cea20b84f378ca284d0fde2ee2cc5
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-07-29 11:46:44 +0700

    Time: 7 ms (38.29%) | Memory: 18.2 MB (66.45%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
@@ -3,12 +3,5 @@ class Solution:
         people.sort( key = lambda x: (-x[0],x[1]))
         ans = []
         for x in people:
-            cnt = 0
-            idx = 0
-            n = len(ans)
-            while cnt!= x[1] and idx < n:
-                if ans[idx][0] >= x[0]:
-                    cnt+=1
-                idx+=1
-            ans.insert(idx,x)
+            ans.insert(x[1],x)
         return ans
