d07b4a9017d71960cfbc26e4d38d19a6881e0d6b
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-08-03 01:23:01 +0700

    Time: 239 ms (5.19%) | Memory: 56.3 MB (5.19%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.py
+++ 2689-rearranging-fruits/rearranging-fruits.py
@@ -1,32 +1,28 @@
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
         m = defaultdict(int)
-        for x,y in zip(basket1,basket2):
-            m[x]+=1
-            m[y]+=1
-        for x in m.values():
-            if x%2:
-                return -1
-        
         mn1 = min(basket1)
         mn2 = min(basket2)
-        ans = 0
         m1 = defaultdict(int)
         m2 = defaultdict(int)
         for x,y in zip(basket1,basket2):
+            m[x]+=1
+            m[y]+=1
             m1[x]+=1
             m2[y]+=1
+        if any(x%2 for x in m.values()):
+            return -1
         store = SortedList()
         for x in m1.keys():
             if m1[x] > m2[x]:
+                temp = min(x,mn1*2,mn2*2)
                 for _ in range((m1[x] - m2[x])//2):
-                    store.add(min(x,mn1*2,mn2*2))
-        #print(m1.keys())
+                    store.add(temp)
         for x in m2.keys():
             if m2[x] > m1[x]:
+                temp = min(x,mn1*2,mn2*2)
                 for _ in range((m2[x] - m1[x])//2):
-                    store.add(min(x,mn1*2,mn2*2))
+                    store.add(temp)
 
-        #print(store)
         return sum(store[:(len(store)//2)])
         
