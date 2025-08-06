d7f6b46d7bb4d2f2eb2e0ef6a3db5a17529d7565
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-08-06 21:38:59 +0700

    Time: 1917 ms (81.21%) | Memory: 81.6 MB (5.37%) - LeetSync

---
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n = len(baskets)-1
+        segs = [0]*4*(n+1)
+        def build(id,l,r):
+            if l == r:
+                segs[id] = baskets[l]
+                return
+            mid = (l+r)//2
+            build(id*2, l, mid )
+            build(id*2 +1, mid+1, r)
+            segs[id] = max(segs[id*2],segs[id*2+1])
+        build(1,0,n)
+
+        def take(id,l,r,x):
+            if segs[id] < x:
+                return False
+            if l == r:
+                if segs[id] >= x:
+                    segs[id] = -inf
+                    return True
+                return False
+            mid = (l+r)//2
+            check = take(id*2,l,mid,x) or take(id*2+1,mid+1,r,x)
+            segs[id] = max(segs[id*2],segs[id*2+1])
+            return check
+        
+        ans = 0
+        for i in fruits:
+            ans+= take(1,0,n,i)
+        return n+1 - ans
