58385c34a6a1949f79d336a06e22d0956992384b
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-08-10 08:19:30 +0700

    Time: 3 ms (59.16%) | Memory: 18 MB (17.56%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        n = str(n)
+        m = defaultdict(int)
+        for i in n:
+            m[i]+=1
+        temp = 1
+        found = False
+        for i in range(32):
+            cur = str(temp)
+            #print(cur)
+            store = m.copy()
+            for x in cur:
+                store[x]-=1
+            found =  all( x == 0 for x in store.values() )
+            if found:
+                #print(cur)
+                return found
+            temp*=2
+            #print(temp)
+        
+        return found
+            
+                    
