db0b8faf7609f2b7727d1647ffd92e22a14628cf
Sony <110512578+Sunauraa@users.noreply.github.com>
2025-08-10 08:20:07 +0700

    Time: 3 ms (59.16%) | Memory: 17.7 MB (79.77%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -8,16 +8,13 @@ class Solution:
         found = False
         for i in range(32):
             cur = str(temp)
-            #print(cur)
             store = m.copy()
             for x in cur:
                 store[x]-=1
             found =  all( x == 0 for x in store.values() )
             if found:
-                #print(cur)
                 return found
             temp*=2
-            #print(temp)
         
         return found
             
