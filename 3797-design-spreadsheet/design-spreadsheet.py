e5fe905faaf6b75f2e5d4a2ac998a38fba3c998b
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-09-18 21:24:21 -0400

    Time: 73 ms (89.02%) | Memory: 23.7 MB (49.77%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.py
+++ 3797-design-spreadsheet/design-spreadsheet.py
@@ -1,32 +1,21 @@
 class Spreadsheet:
 
     def __init__(self, rows: int):
-        self.record = defaultdict(dict)
-        for i in range(26):
-            char = chr( ord('A') + i )
-            for j in range(1,rows+1):
-                self.record[char][j] = 0
+        self.record = defaultdict(int)
 
     def setCell(self, cell: str, value: int) -> None:
-        fi,se = cell[0], int(cell[1:])
-        self.record[fi][se] = value
+        self.record[cell] = value
 
     def resetCell(self, cell: str) -> None:
-        fi,se = cell[0], int(cell[1:])
-        self.record[fi][se] = 0
+        self.record[cell] = 0
 
     def getValue(self, formula: str) -> int:
         formula = formula[1:]
         fin,sen = formula.split('+')
-        #print(fin,sen)
-        #print(ord(fin[0]),ord('A'),ord('Z'))
         if 'A' <= fin[0] <= 'Z':
-            fi1,fi2 = fin[0], int(fin[1:])
-            #print(fi1,fi2)
-            fin = self.record[fi1][fi2]
+            fin = self.record[fin] if self.record[fin] else 0
         if 'A' <= sen[0] <= 'Z':
-            fi1,fi2 = sen[0], int(sen[1:])
-            sen = self.record[fi1][fi2]
+            sen = self.record[sen] if self.record[sen] else 0
         return int(fin) + int(sen)
 
 
