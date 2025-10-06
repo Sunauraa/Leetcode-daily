1c926649a523bf4c18bfe39e97cbdcfc3e7916be
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-10-06 10:05:27 -0400

    Time: 45 ms (27.41%) | Memory: 18.8 MB (12.83%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -10,6 +10,8 @@ class Solution:
         heapq.heappush( q, (dijk[0][0], 0,0) )
         while q:
             temp,u,v = heapq.heappop(q)
+            if u == n-1 and v == m-1:
+                return temp
             if (u,v) in vi:
                 continue
             vi.add((u,v))
@@ -17,8 +19,4 @@ class Solution:
                 nxu,nxv = u + d[k][0],v + d[k][1]
                 if 0 <= nxu < n and 0 <= nxv < m and (nxu,nxv) not in vi:
                     dijk[nxu][nxv] = max(dijk[u][v], grid[nxu][nxv])
-                    if nxu == n-1 and nxv == m-1:
-                        return dijk[nxu][nxv]
                     heapq.heappush(q,(dijk[nxu][nxv], nxu,nxv))
-        
-        return dijk[n-1][m-1]
