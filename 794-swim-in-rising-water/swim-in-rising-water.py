2030152cbb96aae943731b299269c599b149628d
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-10-06 10:03:56 -0400

    Time: 40 ms (33.33%) | Memory: 18.5 MB (53.12%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -5,18 +5,20 @@ class Solution:
         q = []
         d = [[1,0],[0,1],[0,-1],[-1,0]]
         dijk = [[inf]*m for _ in range(n)]
-        vi = [[0]*m for _ in range(n)]
+        vi = set((0,0))
         dijk[0][0] = grid[0][0]
         heapq.heappush( q, (dijk[0][0], 0,0) )
         while q:
             temp,u,v = heapq.heappop(q)
-            if vi[u][v]:
+            if (u,v) in vi:
                 continue
-            vi[u][v] = 1
+            vi.add((u,v))
             for k in range(4):
                 nxu,nxv = u + d[k][0],v + d[k][1]
-                if 0 <= nxu < n and 0 <= nxv < m and not vi[nxu][nxv]:
+                if 0 <= nxu < n and 0 <= nxv < m and (nxu,nxv) not in vi:
                     dijk[nxu][nxv] = max(dijk[u][v], grid[nxu][nxv])
+                    if nxu == n-1 and nxv == m-1:
+                        return dijk[nxu][nxv]
                     heapq.heappush(q,(dijk[nxu][nxv], nxu,nxv))
         
         return dijk[n-1][m-1]
