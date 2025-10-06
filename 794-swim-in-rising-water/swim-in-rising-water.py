c116dc56b9713232f9ece2165081331e9bae71a1
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-10-06 10:05:36 -0400

    Time: 23 ms (84.44%) | Memory: 18.6 MB (23.48%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,22 +1,33 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+        
+
+
+        heap = []
+        heap.append((grid[0][0],0,0))
         n = len(grid)
-        m = len(grid[0])
-        q = []
-        d = [[1,0],[0,1],[0,-1],[-1,0]]
-        dijk = [[inf]*m for _ in range(n)]
-        vi = set((0,0))
-        dijk[0][0] = grid[0][0]
-        heapq.heappush( q, (dijk[0][0], 0,0) )
-        while q:
-            temp,u,v = heapq.heappop(q)
-            if u == n-1 and v == m-1:
-                return temp
-            if (u,v) in vi:
-                continue
-            vi.add((u,v))
-            for k in range(4):
-                nxu,nxv = u + d[k][0],v + d[k][1]
-                if 0 <= nxu < n and 0 <= nxv < m and (nxu,nxv) not in vi:
-                    dijk[nxu][nxv] = max(dijk[u][v], grid[nxu][nxv])
-                    heapq.heappush(q,(dijk[nxu][nxv], nxu,nxv))
+
+        DIRS = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set((0,0))
+
+        while heap:
+            water, row, col = heapq.heappop(heap)
+
+            if row == col == n-1:
+                return water
+
+            for r, c in DIRS:
+                next_r = row + r
+                next_c = col + c
+
+                if next_r < 0 or next_r >= n: continue
+                if next_c < 0 or next_c >= n: continue
+                if (next_r, next_c) in visited: continue
+
+                visited.add((next_r,next_c))
+                next_water_level = max(grid[next_r][next_c], water)
+                heapq.heappush(heap, (next_water_level, next_r, next_c))
+
+        return -1
+
+            
