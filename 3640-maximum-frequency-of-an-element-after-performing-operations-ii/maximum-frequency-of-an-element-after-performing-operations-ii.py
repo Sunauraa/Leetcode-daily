8d72a3e1a164cfe3d0eb2acf8faf454d0e29cb62
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-10-21 21:40:51 -0400

    Time: 877 ms (61.29%) | Memory: 51.4 MB (50.00%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        freq = defaultdict(int)
+        m = defaultdict(int)
+        M = max(nums) + 2
+        for num in nums:
+            freq[num]+=1
+            s,t = max(0, num - k), min(M, num+k+1)
+            m[s]+=1
+            m[t]-=1
+        
+        key = list(m.keys())
+        key.sort()
+        n = len(key)
+        cur = 0
+        cnt = m[key[0]]
+        ans = max(freq.values())
+        #print(m)
+        #print(key)
+        nums.sort()
+        for num in nums:
+            while cur < n - 1 and key[cur+1] <= num:
+                ans = max(ans, min(numOperations,cnt ))
+                cur+=1
+                #print('while',cnt,cur,m[cur])
+                cnt+= m[key[cur]]
+            ans = max(ans, min(numOperations+freq[num], cnt ))
+            #print(cnt,cur,ans)
+        return ans
