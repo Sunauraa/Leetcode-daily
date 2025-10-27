ab40b5b55542d12956c1b02858f509546fca7402
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2025-10-26 20:24:42 -0400

    Time: 11 ms (52.83%) | Memory: 19.5 MB (44.64%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        cur = 0
+        res = 0
+        for s in bank:
+            cnt = s.count('1')
+            if cnt == 0:
+                continue
+            res+= cur*cnt
+            cur = cnt
+        return res
