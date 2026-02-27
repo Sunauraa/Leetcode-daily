bd6d50cf3f93093856941be4e06c06c519088c93
Tuan Son Dao <110512578+Sunauraa@users.noreply.github.com>
2026-02-27 15:26:27 -0500

    Time: 5175 ms (5.33%) | Memory: 27.6 MB (5.33%) - LeetSync

---
+++ 672-bulb-switcher-ii/bulb-switcher-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+        n = min(n,10)
+        dp = [ [0]*( (1 << n)) for _ in range(presses + 1) ]
+        dp[0][0] = 1
+        for i in range(1,presses + 1):
+            #print("presses:", i)
+            for state in range( (1 << (n)) ):
+                # button 1:
+                prevstate = state
+                for j in range(0,n):
+                    prevstate^=(1<<j)
+                if dp[i-1][prevstate]:
+                    dp[i][state] = 1
+                # button 2:
+                prevstate = state
+                for j in range(1,n,2):
+                    prevstate^=(1<<j)
+                if dp[i-1][prevstate]:
+                    dp[i][state] = 1
+                # button 3:
+                prevstate = state
+                for j in range(0,n,2):
+                    prevstate^=(1<<j)
+                if dp[i-1][prevstate]:
+                    dp[i][state] = 1
+                # button 4:
+                prevstate = state
+                for j in range(0,n,3):
+                    prevstate^=(1<<j)
+                if dp[i-1][prevstate]:
+                    dp[i][state] = 1
+                #print(bin(state), dp[i][state])
+        #print()
+        #print(dp[presses])
+        return sum(dp[presses])
